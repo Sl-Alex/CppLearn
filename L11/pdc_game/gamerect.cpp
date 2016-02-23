fcdd912c933b54a4bc904c844e357387d01422b9
Oleksii Slabchenko <alexslabchenko@gmail.com>
2016-02-23 15:51:20 +0200

    Adding missing files

---
+++ L11/pdc_game/gamerect.cpp
@@ -0,0 +1,148 @@
+#include "gamerect.h"
+#include <curses.h>
+
+GameRect::GameRect()
+{
+    //ctor
+}
+
+GameRect::~GameRect()
+{
+    //dtorW
+}
+
+GameRect::GameRect(const GameRect& other)
+{
+    //copy ctor
+}
+
+GameRect& GameRect::operator=(const GameRect& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
+
+void GameRect::drawStep(int stepNumber)
+{
+    if (stepNumber >= stepsCount())
+        return;
+
+    refreshColor();
+
+    int x, y;
+    getRectXY(stepNumber, &x, &y);
+    mvaddch(y,x,m_Char);
+}
+
+void GameRect::drawRect(void)
+{
+    int steps = stepsCount();
+    for (int i = 0; i < steps; i++)
+    {
+        drawStep(i);
+    }
+}
+
+int GameRect::stepsCount(void)
+{
+    return 2*m_Width + 2*m_Height - 2;
+}
+
+void GameRect::refreshColor(void)
+{
+    if (m_Color <= 7)
+    {
+        attron(COLOR_PAIR(m_Color));
+        attroff(A_BOLD);
+    } else {
+        attron(COLOR_PAIR(m_Color - 7) | A_BOLD);
+    }
+}
+
+GameRect::RectSide GameRect::getRectSideOffset(int step, int * offset)
+{
+    if (step == 0)
+    {
+        *offset = 0;
+        return RCT_LEFTTOP;
+    }
+    if (step < m_Width)
+    {
+        *offset = step;
+        return RCT_TOP;
+    }
+    if (step == m_Width)
+    {
+        *offset = 0;
+        return RCT_RIGHTTOP;
+    }
+    step -= m_Width;
+    if (step < m_Height - 1)
+    {
+        *offset = step;
+        return RCT_RIGHT;
+    }
+    if (step == m_Height - 1)
+    {
+        *offset = 0;
+        return RCT_RIGHTBOTTOM;
+    }
+    step -= (m_Height - 1);
+    if (step < m_Width)
+    {
+        *offset = step;
+        return RCT_BOTTOM;
+    }
+    if (step == m_Width)
+    {
+        *offset = 0;
+        return RCT_LEFTBOTTOM;
+    }
+    *offset = step - m_Width;
+    return RCT_LEFT;
+}
+
+void GameRect::getRectXY(int step, int * ret_x, int * ret_y)
+{
+    // step 0 means the left top corner, the rest is clockwise
+    if (step >= stepsCount()) return;
+
+    int offset;
+    RectSide side = getRectSideOffset(step, &offset);
+    switch(side)
+    {
+        case RCT_LEFTTOP:
+            *ret_x = m_X;
+            *ret_y = m_Y;
+            return;
+        case RCT_TOP:
+            *ret_x = m_X + offset;
+            *ret_y = m_Y;
+            return;
+        case RCT_RIGHTTOP:
+            *ret_x = m_X + m_Width;
+            *ret_y = m_Y;
+            return;
+        case RCT_RIGHT:
+            *ret_x = m_X + m_Width;
+            *ret_y = m_Y + offset;
+            return;
+        case RCT_RIGHTBOTTOM:
+            *ret_x = m_X + m_Width;
+            *ret_y = m_Y + m_Height - 1;
+            return;
+        case RCT_BOTTOM:
+            *ret_x = m_X + m_Width - offset;
+            *ret_y = m_Y + m_Height - 1;
+            return;
+        case RCT_LEFTBOTTOM:
+            *ret_x = m_X;
+            *ret_y = m_Y + m_Height - 1;
+            return;
+        case RCT_LEFT:
+            *ret_x = m_X;
+            *ret_y = m_Y + m_Height - 1 - offset;
+            return;
+    }
+}
---
+++ L11/pdc_game/gamerect.h
@@ -0,0 +1,52 @@
+#ifndef GAMERECT_H
+#define GAMERECT_H
+
+
+class GameRect
+{
+    public:
+        GameRect();
+        virtual ~GameRect();
+        GameRect(const GameRect& other);
+        GameRect& operator=(const GameRect& other);
+        int getWidth() { return m_Width; }
+        int getHeight() { return m_Height; }
+        int getX() { return m_X; }
+        int getY() { return m_Y; }
+        int getColor() { return m_Color; }
+        char getChar() {return m_Char; }
+        void setWidth(int val) { m_Width = val; }
+        void setHeight(int val) { m_Height = val; }
+        void setX(int val) { m_X = val; }
+        void setY(int val) { m_Y = val; }
+        void setColor(int val) { m_Color = val; }
+        void setChar(char val) { m_Char = val; }
+
+        void drawStep(int stepNumber);
+        void drawRect();
+        int stepsCount(void);
+    protected:
+    private:
+        enum RectSide{
+            RCT_LEFTTOP,
+            RCT_TOP,
+            RCT_RIGHTTOP,
+            RCT_RIGHT,
+            RCT_RIGHTBOTTOM,
+            RCT_BOTTOM,
+            RCT_LEFTBOTTOM,
+            RCT_LEFT
+        };
+        RectSide getRectSideOffset(int step, int * offset);
+        void getRectXY(int step, int * ret_x, int * ret_y);
+
+        void refreshColor(void);
+        int m_Width;
+        int m_Height;
+        int m_X;
+        int m_Y;
+        char m_Char;
+        int m_Color;
+};
+
+#endif // GAMERECT_H
