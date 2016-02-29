db9891d496dd210983bad1fada5b53302c3232c2
Oleksii Slabchenko <alexslabchenko@gmail.com>
2016-02-29 15:02:55 +0200

    Adding missing spaces

--- L13/text_editor/aboutdialog.cpp
+++ L13/text_editor/aboutdialog.cpp
@@ -12,9 +12,9 @@ AboutDialog::AboutDialog(QWidget * parent)
     header->setProperty("isHeader", true);
     mainLayout->addWidget(header);
     QLabel * text = new QLabel(tr(
-        "This is just a very simple text editor, which was created in order"
-        "to practice a bit with some Qt classes. You can use it at your own"
-        "risk. I'm not responsible if this software will burn your house,"
+        "This is just a very simple text editor, which was created in order "
+        "to practice a bit with some Qt classes. You can use it at your own "
+        "risk. I'm not responsible if this software will burn your house, "
         " spend your money and format your hard disk :).\n\nGood luck!"));
     text->setWordWrap(true);
     mainLayout->addWidget(text);
