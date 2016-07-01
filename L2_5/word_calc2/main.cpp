b745d1dcde8e70e39349698a1657627fe471793d
Oleksii Slabchenko <alexslabchenko@gmail.com>
2016-07-01 06:46:46 +0300

    Word counter update

--- L2_5/word_calc2/main.cpp
+++ L2_5/word_calc2/main.cpp
@@ -4,19 +4,190 @@
 #include <string>
 #include <thread>
 #include <ctime>
-
-#include "wordcounter.h"
-#include "totalcounter.h"
-#include "stringsplitter.h"
-
-WordCounter wc;
-TotalCounter tc;
+#include <map>
+#include <mutex>
+#include <atomic>
 
 void usage(void)
 {
     std::cout << "Usage: word_calc filename" << std::endl;
     std::cout << "  filename - input file name" << std::endl;
 }
+
+typedef struct {
+    char * begin;
+    size_t size;
+} tBuffer;
+
+std::map<std::string, int> wordMap;
+std::vector<tBuffer> rawData;
+std::mutex dataMutex;
+std::atomic<bool> readerDone(false);
+size_t readerTimeStart = 0;
+size_t readerTimeEnd = 0;
+size_t parserTimeStart = 0;
+size_t parserTimeEnd = 0;
+size_t totalTimeStart = 0;
+size_t totalTimeEnd = 0;
+
+void reader(std::string fileName, int bufferSize)
+{
+    readerTimeStart = clock();
+    std::ifstream wordFile(fileName, std::ios::in|std::ios::binary);
+    if (!wordFile.is_open())
+    {
+        std::cout << "Can't open file" << std::endl;
+        readerDone = true;
+        return;
+    }
+
+    while (true)
+    {
+        tBuffer buf;
+
+        char * buffer = new char[bufferSize];
+        buf.begin = buffer;
+        if (!wordFile.read(buffer, bufferSize))
+        {
+            buf.size = wordFile.gcount();
+
+            // Add remaining data to the vector
+            dataMutex.lock();
+            rawData.push_back(buf);
+            dataMutex.unlock();
+
+            break;
+        }
+
+        buf.size = bufferSize;
+
+        // Add block to the vector
+        dataMutex.lock();
+        rawData.push_back(buf);
+        dataMutex.unlock();
+    }
+
+
+    // Done
+    wordFile.close();
+    readerDone = true;
+    readerTimeEnd = clock();
+}
+
+char * findFirstEmpty(char * data, char * end)
+{
+    while (data != end)
+    {
+        if ((*data == ' ') ||
+            (*data == '\n') ||
+            (*data == '\r') ||
+            (*data == '\t'))
+        return data;
+
+        data++;
+    }
+    return 0;
+}
+
+char * findFirstNotEmpty(char * data, char * end)
+{
+    while (data != end)
+    {
+        if ((*data != ' ') &&
+            (*data != '\n') &&
+            (*data != '\r') &
+            (*data != '\t'))
+        return data;
+
+        data++;
+    }
+    return 0;
+}
+
+void parser(void)
+{
+    parserTimeStart = clock();
+    std::string remainder;
+
+    while(true)
+    {
+        // Get buffer pointers
+        dataMutex.lock();
+        if (rawData.size() == 0)
+        {
+            if (readerDone)
+            {
+                dataMutex.unlock();
+                break;
+            }
+            dataMutex.unlock();
+            std::this_thread::yield();
+            continue;
+        }
+        tBuffer buf = rawData.at(0);
+        rawData.erase(rawData.begin());
+        dataMutex.unlock();
+
+        // Parse buffer
+        char * pBegin = buf.begin;
+        char * pEnd = buf.begin;
+
+        if (!remainder.empty())
+        {
+            pBegin = findFirstNotEmpty(pBegin, buf.begin + buf.size);
+            if (pBegin == buf.begin)
+            {
+                std::string second_part;
+                pEnd = findFirstEmpty(pBegin, buf.begin + buf.size);
+                if (pEnd)
+                {
+                    second_part = std::string(pBegin,pEnd);
+                    remainder += second_part;
+                    ++wordMap[remainder];
+                    remainder.clear();
+                }
+                else
+                {
+                    second_part = std::string(pBegin,buf.begin + buf.size);
+                    remainder += second_part;
+                }
+            }
+            else
+            {
+                ++wordMap[remainder];
+                remainder.clear();
+            }
+        }
+
+        while (true)
+        {
+            pBegin = findFirstNotEmpty(pEnd, buf.begin + buf.size);
+            if (pBegin == 0)
+            {
+                break;
+            }
+            else
+
+            pEnd = findFirstEmpty(pBegin, buf.begin + buf.size);
+            if (pEnd == 0)
+            {
+                // Last word without end delimiter
+                remainder = std::string(pBegin, buf.begin + buf.size);
+                break;
+            }
+            else
+            {
+                std::string word = std::string(pBegin, pEnd);
+                ++wordMap[word];
+            }
+        }
+
+        // Release buffer
+        delete[] buf.begin;
+    }
+    parserTimeEnd = clock();
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -30,78 +201,31 @@ int main(int argc, char *argv[])
 
     std::string fileName = argv[1];
 
-    std::ifstream wordFile(fileName, std::ios::in|std::ios::binary);
-    if (!wordFile.is_open())
-    {
-        std::cout << "Can't open file" << std::endl;
-        return EIO;
-    }
-
-    char * buffer = new char [1024000];
-
-    size_t start1, end1;
-    size_t start2, end2;
-
-    size_t time1 = 0;
-    size_t time2 = 0;
-
-    std::thread thread1;
-    std::thread thread2;
-
-    wc.start();
-    tc.start();
-
     std::cout << "Processing..." << std::endl;
-    while(true)
-    {
-        // file read begin
-        start1 = clock();
-        if (!wordFile.read(buffer,1024))
-        {
-            end1 = clock();
-            time1 += end1 - start1;
-            // file read end
-            break;
-        }
-        end1 = clock();
-        time1 += end1 - start1;
-        // file read end
 
-        // parse begin
-        start2 = clock();
+    totalTimeStart = clock();
+    std::thread thread1 = std::thread(&reader, fileName, 1024);
+    std::thread thread2 = std::thread(&parser);
 
-
-
-        static bool skip = true;
-        if (!skip)
-        {
-            thread1.join();
-            thread2.join();
-        }
-        skip = false;
-
-        std::string line(buffer);
-
-        thread1 = std::thread(&wc.addBlock, &wc, line);
-        thread2 = std::thread(&tc.addBlock, &tc, line);
-
-        end2 = clock();
-        time2 += end2 - start2;
-        // parse end
-    }
     thread1.join();
     thread2.join();
 
-    wc.finish();
-    tc.finish();
+    totalTimeEnd = clock();
 
-    wordFile.close();
+    std::cout << "Done." << std::endl;
 
-    std::cout << "Result - " << tc.count() << " word(s)" << std::endl;
-    std::cout << "         " << wc.count() << " unique word(s)" << std::endl;
-    std::cout << "I/O time:        " << time1 << std::endl;
-    std::cout << "Processing time: " << time2 << std::endl;
-    std::cout << "Total time:      " << time1 + time2 << std::endl;
+    std::ofstream ofs ("result.txt", std::ofstream::out);
+
+    for (auto it = wordMap.begin(); it != wordMap.end(); it++)
+    {
+        ofs << "(" << it->first << ") (" << it->second << ")" << std::endl;
+    }
+
+    ofs.close();
+
+    std::cout << "Reader time: " << readerTimeEnd - readerTimeStart << std::endl;
+    std::cout << "Parser time: " << parserTimeEnd - parserTimeStart << std::endl;
+    std::cout << "Total time:  " << totalTimeEnd - totalTimeStart << std::endl;
 
     return EXIT_SUCCESS;
 }
--- L2_5/word_calc2/stringsplitter.cpp
+++
@@ -1,6 +0,0 @@
-#include "stringsplitter.h"
-
-StringSplitter::StringSplitter()
-{
-
-}
